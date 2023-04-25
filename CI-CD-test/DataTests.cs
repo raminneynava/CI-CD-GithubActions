f9429aa59a86f9f2fd94189177cca2d839c9215d
Barayand-God <Barayand-God@DESKTOP-FC5GOLP>
2023-04-25 14:30:06 +0430

    new3

--- CI-CD-test/DataTests.cs
+++ CI-CD-test/DataTests.cs
@@ -7,7 +7,7 @@ namespace CI_CD_test
         [Fact]
         public void Number_Of_Countries()
         {
-            Assert.Equal(2, Countries.countryList.Count());
+            Assert.Equal(3, Countries.countryList.Count());
         }
     }
 }
